2ab3f35da3a309c775dbd9ac0cdfd70f05b0e759
miguel celma <mrcelma3@gmail.com>
2022-04-18 06:06:33 +0200

    keyboard set

--- src/App.js
+++ src/App.js
@@ -8,12 +8,33 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
+  const onSelectLetter = (keyVal) =>{
+    if(currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+
+  }
+  const onDelete = () =>{
+    if(currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1});
+
+  }
+  const onEnter = () =>{
+    if (currAttempt.letterPos !==5) return;
+    setCurAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+  }
   return (
     <div className="App">
      <nav>
        <h1>Wordle Project</h1>
        </nav>
-       <AppContext.Provider value={{board, setBoard}}>
+       <AppContext.Provider value={{board, setBoard,currAttempt, setCurrAttempt,onSelectLetter,onDelete,onEnter}}>
         <div className='game'>
           <Board />
           <Keyboard />
